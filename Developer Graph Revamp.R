3e92410f2e213d6dc6e3c97fe26871804ee7e0bd
Max Woolf <max@minimaxir.com>
2014-01-20 07:51:01 -0800

    really correct

--- Developer Graph Revamp.R
+++ Developer Graph Revamp.R
@@ -6,7 +6,7 @@ library("grid")
 font <- "Chalkduster"	# font: change if not on Mac
 font_axis <- "Avenir Next Condensed Medium"
 themecolor <- "#FAFAFA"
-themebg <- "#333333"
+themebg <- "#222222"
 
 theme_chalk <- function() {theme_bw(base_size = 6) +
 theme(text = element_text(color = themecolor, family=font),
@@ -15,10 +15,13 @@ panel.border = element_rect(color = themecolor),
 plot.margin = unit(c(0.1,0.1,0.1,0.1), "cm"),
 panel.background=element_rect(fill = themebg),
 plot.background=element_rect(fill = themebg),
-panel.grid=element_blank())                                     
+panel.grid=element_blank(),
+title = element_text(family=font_axis,size=8))                                     
 }
 
-df <- data.frame(lang=c("Java","PHP","C#","Android","JavaScript","Rails","Python","iOS",".net","C++"),perc=c(0.2226,0.1153,0.1074,0.0994,0.0923,0.0830,0.0829,0.0753,0.0722,0.0496))
+df <- data.frame(lang=c("Java","PHP","C#","Android","JavaScript","Rails","Python","iOS",".net","C++"),
+perc=c(0.2226,0.1153,0.1074,0.0994,0.0923,0.0830,0.0829,0.0753,0.0722,0.0496))
+
 df$lang <- reorder(df$lang,df$perc)  # internally sort lang by perc ascending
 
 
@@ -30,7 +33,7 @@ scale_y_continuous(labels = percent) +	# percent scale
 theme_chalk() +	# render theme
 theme(axis.title.y = element_text(family=font_axis), axis.title.x =  element_blank() ,axis.ticks.x = element_blank(),axis.text.x= element_blank()) +	# tweak theme
 geom_text(label=df$lang, vjust=-1.0, color=themecolor, family=font, size=1.5) + # add annotations
-ylab(toupper("Percent Among Top 10 Searches"))
+labs(y = toupper("Percent Among Top 10 Searches"), title=toupper("Top 10 Most In Demand Developer Skills of 2013"))
 
 ggsave("dev-graph-1.png",height=3,width=4)	# produces 1200x900px image
 
@@ -42,8 +45,8 @@ scale_y_continuous(labels = percent) +
 coord_flip() +	# convert columns to bars
 theme_chalk() +
 theme(axis.title.x = element_text(family=font_axis), axis.title.y = element_blank(), axis.ticks.y = element_blank(),axis.text.y= element_blank()) +
-geom_text(label=df$lang, hjust=-0.25, color=themecolor, family=font, size=1.5) +
-ylab(toupper("Percent Among Top 10 Searches"))
+geom_text(label=df$lang, hjust=-0.25, color=themecolor, family=font, size=2) +
+labs(y = toupper("Percent Among Top 10 Searches"), title=toupper("Top 10 Most In Demand Developer Skills of 2013"))
 
 ggsave("dev-graph-2.png",height=3,width=4)
 
@@ -57,6 +60,7 @@ scale_y_continuous(labels = percent) +
 coord_flip() +
 theme_chalk() +
 theme(axis.title.x = element_blank(), axis.title.y =  element_blank() ,axis.ticks.y = element_blank(),axis.ticks.x = element_blank(),axis.text.x = element_blank()) +
-geom_text(label=perc_text, hjust=1.5, color=themecolor, family=font, size=1.5)
+geom_text(label=perc_text, hjust=1.5, color=themecolor, family=font, size=2) +
+labs(title=toupper("Top 10 Most In Demand Developer Skills of 2013"))
 
 ggsave("dev-graph-3.png",height=3,width=4)
